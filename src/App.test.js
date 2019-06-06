a077d42a6a26ddd89483d9d45a85874f49aaad9c
Shivraj Rath <Shivraj.Rath6@T-Mobile.com>
2019-06-06 13:33:48 -0700

    unit tests for app component

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,21 @@ import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
 import Search from "./components/Search";
+import IconContainer from "./components/IconContainer";
+import Carousel from "./components/Carousel";
 
 const wrapper = shallow(<App />);
 
 describe("<App />", () => {
-  it("renders three <Search /> component", () => {
+  it("renders one <Search /> component", () => {
     expect(wrapper.find(Search).length).toEqual(1);
+  });
+
+  it("renders one <IconContainer /> component", () => {
+    expect(wrapper.find(IconContainer).length).toEqual(1);
+  });
+
+  it("renders one <Carousel /> component", () => {
+    expect(wrapper.find(Carousel).length).toEqual(1);
   });
 });

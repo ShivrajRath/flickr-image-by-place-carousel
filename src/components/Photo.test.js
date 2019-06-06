bfa5bafd718acbfe18ea77ef4e193bf8661c3982
Shivraj Rath <Shivraj.Rath6@T-Mobile.com>
2019-06-06 14:35:31 -0700

    unit tests for Photo component

---
+++ src/components/Photo.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Photo from "./Photo";
+import { shallow } from "enzyme";
+
+describe("<Photo />", () => {
+  describe("Hidden Photo", () => {
+    const props = {
+      photo: { url: "test_url", title: "test_title" },
+      isVisible: false
+    };
+
+    const wrapper = shallow(<Photo {...props} />);
+
+    it("should have hidden class", () => {
+      expect(wrapper.find("figure").hasClass("current")).toBeFalsy();
+      expect(wrapper.find("figure").hasClass("hidden")).toBeTruthy();
+    });
+  });
+
+  describe("Visible Photo", () => {
+    const props = {
+      photo: { url: "test_url", title: "test_title" },
+      isVisible: true
+    };
+
+    const wrapper = shallow(<Photo {...props} />);
+
+    it("should have current class", () => {
+      expect(wrapper.find("figure").hasClass("hidden")).toBeFalsy();
+      expect(wrapper.find("figure").hasClass("current")).toBeTruthy();
+    });
+
+    it("image should have url and alt", () => {
+      expect(wrapper.find("img").prop("src")).toEqual("test_url");
+      expect(wrapper.find("img").prop("alt")).toEqual("test_title");
+    });
+
+    it("fig caption should have title text", () => {
+      expect(wrapper.find("figcaption").text()).toEqual("test_title");
+    });
+  });
+});
